f0ae9f2dd49f17397500da0b51df0d31a0a8246a
Megh Bhalerao <megh.bhalerao@gmail.com>
2023-11-13 02:18:16 +0000

    general push

--- run_c10_herding.sh
+++ run_c10_herding.sh
@@ -1,4 +1,4 @@
 for frac in 0.001 0.005 0.01 0.05 0.1 0.2 0.3
 do
-CUDA_VISIBLE_DEVICES=2 python -u main.py --fraction 0.1 --dataset CIFAR10 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Herding --model ResNet9 --lr 0.1 -sp ./result --batch 128
+CUDA_VISIBLE_DEVICES=3 python -u main.py --fraction $frac --dataset CIFAR10 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Herding --model ResNet9 --lr 0.1 -sp ./result --batch 128
 done
