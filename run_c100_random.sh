1e753754928b142c3da558717fa46ad1cd44257c
Megh Bhalerao <megh.bhalerao@gmail.com>
2023-11-14 07:08:27 +0000

    general push

--- run_c100_random.sh
+++ run_c100_random.sh
@@ -2,28 +2,28 @@
 
 # Define your four functions
 function loop1() {
-    for frac in 0.001 0.005 0.01 
+    for frac in 0.01 0.05
         do
         CUDA_VISIBLE_DEVICES=0 python -u main.py --fraction $frac --dataset CIFAR100 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Uniform --model ResNet9 --lr 0.1 -sp ./result --batch 128
         done
 }
 
 function loop2() {
-    for frac in  0.05 0.1 
+    for frac in  0.1 0.2 0.3
         do
         CUDA_VISIBLE_DEVICES=1 python -u main.py --fraction $frac --dataset CIFAR100 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Uniform --model ResNet9 --lr 0.1 -sp ./result --batch 128
         done
 }
 
 function loop3() {
-    for frac in 0.2 0.3 0.4
+    for frac in 0.4 0.5 0.6
         do
         CUDA_VISIBLE_DEVICES=2 python -u main.py --fraction $frac --dataset CIFAR100 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Uniform --model ResNet9 --lr 0.1 -sp ./result --batch 128
         done
 }
 
 function loop4() {
-    for frac in  0.5 0.6 0.9
+    for frac in  0.7 0.8 0.9
         do
         CUDA_VISIBLE_DEVICES=3 python -u main.py --fraction $frac --dataset CIFAR100 --data_path ~/datasets --num_exp 5 --workers 10 --optimizer SGD -se 10 --selection Uniform --model ResNet9 --lr 0.1 -sp ./result --batch 128
         done
